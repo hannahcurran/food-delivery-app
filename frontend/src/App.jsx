379ac15e66a2a42a82b2b246fffaed0f3687cd59
hannah <hannah.l.curran@gmail.com>
2024-03-20 15:45:37 +0000

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import NavBar from './components/NavBar/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar />
+
+      <Routes>
+        < Route path='/' element={<Home />} />
+        <Route path='/' element={<Cart />} />
+        <Route path='/' element={<PlaceOrder />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
